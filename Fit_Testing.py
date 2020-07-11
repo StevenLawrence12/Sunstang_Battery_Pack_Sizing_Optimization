60a71a7c0145a1fa4563e2d0da59c6217357a26a
Steven Lawrence <slawre43@uwo.ca>
2020-07-11 17:40:32 -0600

    Calculated average velocity, average battery consumption, and total battery consumption error which was then stored in an array

--- Fit_Testing.py
+++ Fit_Testing.py
@@ -5,3 +5,7 @@ import glob
 path = r'D:\.Steven Data\Extracurricular\Sunstang\2020-2021\Strategy\Code'
 filenames = glob.glob(path + "/WSC Energy*.csv")
 dfs = [pd.read_csv(f) for f in filenames]
+
+a = np.array([['Data Set #', 'Average Velocity (km/h)', 'Average Battery Energy Consumption (kWh)', 'Total Energy Error']])
+for x in range(len(filenames)):
+    a = np.append(a, [[x, dfs[x]['Segment Velocity (km/h)'].mean(), dfs[x]['Battery Energy Consumption (kWh)'].mean(), dfs[x]['Energy Difference'].sum()]], axis = 0)
