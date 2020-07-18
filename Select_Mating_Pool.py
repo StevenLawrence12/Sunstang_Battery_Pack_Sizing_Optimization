31139fcfa4185b9db34f671aef7bb16b9f35f4b5
Steven Lawrence <slawre43@uwo.ca>
2020-07-18 17:26:06 -0600

    Added ability to read in velocity value of parents selected for breeding from each csv file and output into its own csv file

--- Select_Mating_Pool.py
+++ Select_Mating_Pool.py
@@ -1,20 +1,25 @@
 import numpy as np
 import pandas as pd 
 
-dfs = pd.read_csv('Generation Genotypes.csv')
+GG_df = pd.read_csv('Generation Genotypes.csv')
 #print(dfs)
 
 num_parents = 4
-parents = np.empty((num_parents, dfs['Data Set #'].shape[0]))
+parents_vel_df = []
 
 for parent_num in range(num_parents):
 
-    max_fitness_idx = np.where(dfs['ASC Score'] == np.max(dfs['ASC Score']))
+    max_fitness_idx = np.where(GG_df['ASC Score'] == np.max(GG_df['ASC Score']))
 
     max_fitness_idx = max_fitness_idx[0][0]
     print(max_fitness_idx)
+    parents_vel_df.append(pd.read_csv(f'WSC Energy Management Model({max_fitness_idx}).csv', usecols = ['Segment Velocity (km/h)']))
+    
+    GG_df['ASC Score'][max_fitness_idx] = -99999999999
 
-    #parents[parent_num, :] = dfs[max_fitness_idx, :]
 
-    dfs['ASC Score'][max_fitness_idx] = -99999999999
+parents = pd.concat(parents_vel_df, axis = 1, ignore_index = True)
+print(parents)
+
+parents.to_csv('Breeding_Parents.csv', index = False, header = False)
 
