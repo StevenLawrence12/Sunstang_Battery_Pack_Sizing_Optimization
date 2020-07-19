219073457cdb4a3720673900a0f628875dbd6f25
Steven Lawrence <slawre43@uwo.ca>
2020-07-19 10:59:05 -0600

    Started changing calculations to use vecorized operations with numpy to improve performance. Added additional code to calculate Aero,rolling,and gravitiational power. Calculated time difference for each segment.

--- Battery_Pack_Sizing_Optimization.py
+++ Battery_Pack_Sizing_Optimization.py
@@ -1,3 +1,7 @@
+# import timeit
+# code_to_test = """
+import time
+start_time = time.time()
 import math
 import datetime
 import random
@@ -22,6 +26,9 @@ def Array_Power(Day, Latitude, Time, Sunrise, DayLength, Pmax, Driving): #Calcul
 
 def Grav_Power(V,M,d,alt1,alt2): #Calculate the power loss due to gravitatational effects
     return V*M*9.81*math.sin(math.atan((alt2-alt1)/1000/d))/3.6
+
+def Grav_Power_np(V,M,d,alt1,alt2): #Calculate the power loss due to gravitatational effects
+    return V*M*9.81*np.sin(np.arctan((alt2-alt1)/1000/d))/3.6
 
 def Haversine(lat1, lat2, long1, long2, r):
     lat1=math.radians(lat1)
@@ -151,24 +158,34 @@ def Generate_Data(Route_Data_csv):
     "Gravitaional Power (W)","Kinetic Power (W)", "Battery Power (W)", "Battery Energy Consumption (kWh)"]
     
     Dataset_num = 0     #Counter for the data set number
-    Req_Datasets = int(input("How many datasets do you need? "))
-    
+    # Req_Datasets = int(input("How many datasets do you need? "))
+    Req_Datasets = 10
+
     #Read route data csv file
     Route_Data_df = pd.read_csv(Route_Data_csv)     #Load in route data to a dataframe
-    
+    Route_Data_arr = Route_Data_df.to_numpy()
+    # Route_Data_arr = np.hsplit(Route_Data_arr,3)
+    # lat_arr = Route_Data_arr[0]
+    # long_arr = Route_Data_arr[1]
+    # alt_arr = Route_Data_arr[2]
 
     Vel_df = pd.read_csv(Output_path + "\Inital_Velocities.csv")
+    Vel_arr = Vel_df.to_numpy()
+    
+
+
     Num_Segment = Vel_df.shape[0] #calculate the number of race route segments
+    print(Route_Data_arr[0:Num_Segment,2].reshape(Num_Segment,1))
+
 
     SR = sunrise(Route_Data_df['latitude'][0], Route_Data_df['longitude'][0], timezone, Time.date())
     for x in range(Num_Segment): 
         Velocity.append(random.randrange(25,88))  #Solar Car Speed **This needs to be a list/array with a size of the number of data points along the route - 1]
-    
     #Calculate Powers
     for x in range(Num_Segment):
         Seg_Dist.append(Haversine(Route_Data_df['latitude'][x],Route_Data_df['latitude'][x+1], Route_Data_df['longitude'][x], Route_Data_df['longitude'][x+1], 6371))
         dT.append(delta_T(Velocity[x],Seg_Dist[x]))
-    
+
     
         End_Time = Time+dT[x]
         if End_Time.hour == 18:
@@ -188,13 +205,27 @@ def Generate_Data(Route_Data_csv):
     
         Time = SET[x]
     
+    Seg_Dist_arr = np.tile(np.reshape(np.asarray(Seg_Dist),(Num_Segment,1)),Req_Datasets)
+
+    #--------------START NUMPY--------------------
+    
+    dT_arr = Vel_arr/Seg_Dist_arr*3600
+    Ap_arr = Aero_Power(A, Cd, p, Vel_arr)
+    Rr_arr = Roll_Resist(Crr, Vel_arr, Loaded_Weight)
+    Gp_arr = Grav_Power_np(Vel_arr, Loaded_Weight, Seg_Dist_arr, Route_Data_arr[0:Num_Segment,2].reshape(Num_Segment,1), Route_Data_arr[1:Route_Data_arr.shape[0],2].reshape(Num_Segment,1))
+    print(Vel_arr[0][0])
+    print(Seg_Dist_arr[0][0])
+    print(Gp_arr[0][0])
+
+    #---------------END NUMPY---------------------
+
     #Code to change power values for each new data set
     while Dataset_num != Req_Datasets:
         Time = datetime.datetime.fromisoformat(Start_Day)       #create datetime object from the Stat_Day str
         for x in range(Num_Segment): 
             Velocity[x] = random.randrange(25,120)
         for x in range(Num_Segment):
-            Seg_Dist[x] = Haversine(Route_Data_df['latitude'][x],Route_Data_df['latitude'][x+1], Route_Data_df['longitude'][x], Route_Data_df['longitude'][x+1], 6371)
+            # dT[x] = delta_T(Vel_arr[x][Dataset_num],Seg_Dist[x])
             dT[x] = delta_T(Velocity[x],Seg_Dist[x])
     
             End_Time = Time+dT[x]
@@ -206,10 +237,15 @@ def Generate_Data(Route_Data_csv):
         
             Power_Array[x] = Array_Power(Time.timetuple().tm_yday,Route_Data_df['latitude'][x],conv_to_DT(Time.hour,Time.minute,Time.second,Time.microsecond),conv_to_DT(SR.hour,SR.minute,SR.second,
             SR.microsecond), DL.total_seconds()/3600,Max_Array_Power,1)
+            # Power_Drag[x] = Aero_Power(A, Cd, p, Vel_arr[x][Dataset_num])
+            # Power_Roll[x] = Roll_Resist(Crr, Vel_arr[x][Dataset_num], Loaded_Weight)
+            # Power_Grav[x] = Grav_Power(Vel_arr[x][Dataset_num], Loaded_Weight, Seg_Dist[x], Route_Data_df['altitude'][x], Route_Data_df['altitude'][x+1])
+            # Power_Kine[x] = Kine_Power(Vel_arr[x][Dataset_num], Vel_arr[x-1][Dataset_num], Seg_Dist[x], Loaded_Weight, SST[x])
             Power_Drag[x] = Aero_Power(A, Cd, p, Velocity[x])
             Power_Roll[x] = Roll_Resist(Crr, Velocity[x], Loaded_Weight)
             Power_Grav[x] = Grav_Power(Velocity[x], Loaded_Weight, Seg_Dist[x], Route_Data_df['altitude'][x], Route_Data_df['altitude'][x+1])
             Power_Kine[x] = Kine_Power(Velocity[x], Velocity[x-1], Seg_Dist[x], Loaded_Weight, SST[x])
+
             Power_batt[x] = Batt_Power(Power_Drag[x], Power_Roll[x], Power_Grav[x], Power_Kine[x], Power_Array[x], MotEff, Power_elec)
             Energy_batt[x] = Energy(Power_batt[x], dT[x].total_seconds()/3600)
     
@@ -229,6 +265,12 @@ def Generate_Data(Route_Data_csv):
         
  #Data sets
 
-Output_path = r'D:\.Steven Data\Extracurricular\Sunstang\2020-2021\Strategy\Code\Output_Data'
-Route_Data_csv = input("Which competition route dataset would you like to input? ")
+
+Output_path = r'D:\.Steven Data\Extracurricular\Sunstang/2020-2021\Strategy\Code\Output_Data'
+# Route_Data_csv = input("Which competition route dataset would you like to input? ")
+Route_Data_csv = "WSC_Route_Data.csv"
 Generate_Data(Route_Data_csv)
+print(time.time()-start_time)
+
+# elapsed_time = timeit.timeit(code_to_test)
+# print(elapsed_time)
